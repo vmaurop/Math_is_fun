771aff5dfd85a7c00088749bd9a90c6a1b77db41
Mavropoulos Evangelos <vmaurop@gmail.com>
2017-10-07 21:51:37 +0300

    Update Calculations.java

--- Calculations.java
+++ Calculations.java
@@ -19,53 +19,61 @@ public class Calculations
        String str = String.format("Current Date/Time :"+date );
        System.out.println(str); 
      
-    // Statistic.stat();
-    //  Gcd_Lcm x = new Gcd_Lcm();
-     //  x.gdt_and_lcm();
+    /*       Gcd_Lcm k = new Gcd_Lcm();
+                k.gdt_and_lcm();
         
             
-        
-        //  Probabilities.random();
+          Statistic.stat();
+          
+          
+          Probabilities.random();
         
          
-          // Mathematics x = new Mathematics();
-          // x.exponential_roots(2, 10);
-        
-          // System.out.println(""+Mathematics.factorial(100));
-         
-          //   Mathematics.exponential_equation(0,3);
-             //  Mathematics.exponent_root(2,9);
-             
-             
-           //   Mathematics.equationX1(0,0);
            
-            
-            
-            
+          
+          Mathematics d = new Mathematics();
+            d.exponential_roots(2, 10); 
+            System.out.println(""+Mathematics.factorial(100));     
+            d.exponential_equation(0,3);
+            d.exponent_root(2,9);
+            d.equationX1(0,0);
+            Mathematics.even_odd(2);
+            d.gtd_an();
+          
+           
          
-          // Euler1 x = new Euler1();
-             //   x.palindrome();
-            //  x.primary_nymbers();
-             //  x.multiples_of_3_5();
-             // x.primary_nymbers();
-            // x.sum_square_difference();
-              // x.Even_Fibonacci_numbers();
-              // x.smallest_multiple();
-              //   x.special_pythagorean_triplet();
-              // x.power_digit_sum();
-             // x.factorial_digit_sum();
-            // x.large_sum();
-            //  x.selfpowers();
-              //x.powerfuldigitsum();
-             // x.Large_non_Mersenne_prime();
-           //  Euler2 y = new Euler2();
-           //  y.distinctpowers();
-       // y.digit_1000_Fibonacci_number();
-        
-          //HackerRank1 w = new HackerRank1();
-          //w.Mini_Max_Sum();
+               Euler1 x = new Euler1();
+               x.palindrome();
+               x.primary_nymbers();
+               x.multiples_of_3_5();
+               x.primary_nymbers();
+               x.sum_square_difference();
+               x.Even_Fibonacci_numbers();
+               x.smallest_multiple();
+               x.special_pythagorean_triplet();
+               x.power_digit_sum();
+               x.factorial_digit_sum();
+               x.large_sum();
+               x.selfpowers();
+               x.powerfuldigitsum();
+               x.Large_non_Mersenne_prime();
+              
+              
+              
+           Euler2 y = new Euler2();
+                y.distinctpowers();
+                y.digit_1000_Fibonacci_number();
+          
+          
+           HackerRank1 w = new HackerRank1();
+                w.Mini_Max_Sum();
             
-                    
+          */          
     }
     
 }
+
+             
+            
+            
+         
